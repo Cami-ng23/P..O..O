07c821698e39c654ac58519778bfce58684c71ed
Cami-ng23 <deiberdiaz@liceovvh.cl>
2026-03-17 12:28:48 -0300

    KOKOO

---
+++ 02_condiciones/static/js/script.js
@@ -0,0 +1,216 @@
+/*1. El Portero Digital
+Crea una variable edad. Si la edad es 18 o más, 
+muestra por consola: 
+"Acceso permitido a la App". 
+Si es menor, muestra: "Acceso denegado: necesitas ser mayor de edad".
+ */
+
+function digital() {
+    let edad = parseInt(prompt("ingresar edad:"));
+    if (edad >= 18) {
+        alert("Acceso permitido a la app")
+    }
+    else if (edad <= 0 && edad >= 120) {
+        alert("Ingrese una edad valida:")
+    } else {
+        alert("Acceso denegado: necesita ser mayor de edad.")
+    }
+}
+
+____________________________________________________________________________
+
+/*2. Validador de Nombres
+Declara una variable nombre.
+Si el nombre no está vacío, crea un arreglo llamado usuarios y agrega el nombre usando .push().
+Si está vacío, muestra: "Error: El nombre no puede estar en blanco".
+ */
+
+
+function nombre() {
+    let nombre = parseInt(prompt("Ingresar nombre:"));
+    let usuarios = [];
+
+    if (nombre.length > 0) {
+        usuarios.push(nombre);
+        alert(usuarios)
+    } else {
+        alert("error: el nombre no puede estar en blanco");
+    }
+}
+
+
+____________________________________________________________________________
+
+/*3. El Filtro de Duplicados
+Tienes esta lista: let productos = ["celular", "teclado", "mouse"];
+Declara una variable nuevoProducto.
+Si el producto no está en la lista (usa .includes()), agrégalo al final y muestra la lista completa.
+Si ya existe, muestra: "El producto ya está en el inventario".
+ */
+
+function duplicado() {
+    let productos = ["celular", "teclado", "mouse"]
+    let nuevoProducto = "celular";
+
+    if (productos.includes(nuevoProducto)) {
+        alert("El producto ya esta en el inventario");
+    } else {
+        alert("El producto no esta en el inventario");
+    }
+}
+
+____________________________________________________________________________
+
+/*4. Control de Stock Máximo
+Imagina que una bodega solo tiene espacio para 5 cajas.
+let bodega = ["caja1", "caja2", "caja3", "caja4", "caja5", "caja6"];
+Si el tamaño de la bodega (.length) es mayor a 5, elimina la última caja con .pop() y muestra: "Bodega llena, eliminando último ingreso".
+Si es 5 o menos, muestra: "Espacio disponible".
+ */
+let bodega = ["caja1", "caja2", "caja3", "caja4", "caja5", "caja6"];
+function controlStock() {
+    if (bodega.length <= 5) {
+        alert("Espacio disponible")
+    } else {
+        alert("Bodega llena, eliminando ultimo ingreso")
+        bodega.pop();
+    }
+}
+
+
+
+____________________________________________________________________________
+
+/*5. Limpieza de Datos
+A veces recibimos datos basura. let colaEspera = ["error_404", "Juan", "Sofía"];
+Si el primer elemento (índice 0) es igual a "error_404", elimínalo usando .shift() y muestra la cola limpia.
+Si no, muestra: "La lista de espera está correcta".
+ */
+function operarIntruso() {
+    let colaEspera = ["Error_404", "Juan", "Sofia"];
+    if (colaEspera[0] == "error_404") {
+        colaEspera.shift()
+    }
+    alert(`Cola de espera limpia: ${colaEspera.join(" - ")}`)
+}
+
+____________________________________________________________________________
+
+/*6. Acceso VIP
+Tienes una lista de usuarios comunes: let foro = ["User1", "User2"];
+Declara la variable rol.
+Si el rol es "Admin", agrega el nombre al inicio de la lista con .unshift() para que tenga prioridad.
+Si no es Admin, agrégalo al final con .push().
+ */
+
+
+function accesoVip() {
+    let foro = ["User1", "User2"];
+    let rol = parseInt(prompt("Ingresar rol:"));
+    if (rol.tolowercase === "admin") {
+        foro.unshift(rol)
+        alert(foro)
+    } else {
+        foro.push(rol);
+        alert(foro);
+    }
+}
+
+
+____________________________________________________________________________
+
+/*7. Sistema de Calificaciones Chile (Escala 1 a 7)
+Crea una variable nota.
+Si la nota es 6 o superior: "Excelente, eximido".
+Si la nota es entre 4 y 5.9: "Aprobado, vas a examen".
+Si es menor a 4: "Reprobado".
+(Pista: Usa else if).
+ */
+
+function calificaciones() {
+    let nota = parseFloat(prompt("Ingrese la nota: "));
+    if (nota >= 6 && nota <= 7) {
+        alert("Excelente, eximido")
+    }
+    else if (nota >= 4 && nota <= 5.9) {
+        alert("aprobado, vas a examen ")
+    }
+    else if (nota < 4 && nota >= 1) {
+        alert("Reprobado")
+    }
+    else {
+        alert("Ingrese un valor valido")
+    }
+}
+
+____________________________________________________________________________
+
+/*8. Buscador de Invitados
+let invitados = ["Ana", "Luis", "Camila", "Diego"];
+Crea una variable persona.
+Si la persona está en la lista, muestra: "¡Bienvenido/a [nombre]! Pasa a la fiesta".
+Si no está, muestra: "Lo siento, no estás en la lista de invitados".
+ */
+
+function buscador() {
+    let invitados = ["ana", "Luis", "camila", "Diego"];
+    let personas = prompt("Ingrese su nombre : ");
+    if (invitados.includes(personas)) {
+        alert(`¡Bienvenido/a ${personas}! pasa a la fiesta`);
+    }
+    else {
+        alert("Lo siento, no estas en la lista de invitaados")
+    }
+}
+
+/*
+Imagina una estantería con dos niveles (una matriz):
+let estanteria = [ ["Manzanas", "Peras"], ["Leche", "Yogur"] ];
+Crea una variable seccion (0 o 1) y una variable producto.
+Accede a la sección elegida. Si el producto solicitado está en esa sub-lista, muestra: "Producto encontrado en el estante".
+Si no, muestra: "No tenemos ese producto en esa sección".
+
+*/
+
+function almacen() {
+    let estanteria = [["Manzanas", "Peras"], ["Leche", "Yogur"]];
+    let seccion = prompt("Ingresar una seccion (0 o 1)");
+    let productos = prompt("Ingresar producto solicitado");
+    if (estanteria[seccion].includes(productos)) {
+        alert("Produto encontrado en el estante")
+    } else {
+        alert("No tenemos ese producto en esa seccion")
+    }
+}
+
+/*
+Crea un arreglo vacío llamado bitacora.
+Declara las variables nombreVisita y esVip (booleano).
+Si el nombre está vacío: Mostrar error.
+Si el nombre existe y esVip es true: Agregarlo al inicio con .unshift().
+Si el nombre existe y esVip es false: Agregarlo al final con .push().
+Al final, muestra cuántas personas hay en total usando .length.
+*/
+let bitacora = []
+
+function visitas() {
+    let nombreVisita = prompt("Ingresa tu nombre :");
+    let esVip = parseInt(prompt(`¿Eres VIP?\n0 = no\n1 = si`));
+    nombreVisita = nombreVisita.trim(); // elimina los espacios
+    if (nombreVisita == "") {
+        alert("Error : El nombre no puede eatar vacio")
+    } else if (bitacora.includes(nombreVisita)) {
+        alert("Esa persona ya esta en la lista");
+    } else if (esVip == 1) {
+        bitacora.unshift(nombreVisita);
+        alert(`Agregado correctamente a la lista:\n${bitacora.join(",")}\nPersonas en total:
+        ${bitacora.length}`)
+    } else if (esVip == 0) {
+        bitacora.push(nombreVisita);
+        alert(`Agregado correctamente a la lista:\n${bitacora.join(",")}\nPersonas en total:
+        ${bitacora.length}`)
+    } else {
+        alert("Ingrese valores validos.")
+    }
+
+}
